acdee6a04b13f5d6b8c3c0de5eeb989b1176c7a4
MyeongchanJin <jmcchan@naver.com>
2020-07-16 10:59:12 +0900

    Class Component로 변경

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function tick() {
+class Clock extends Component {
 
-    const element = <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+        };
+    }
 
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
+    componentDidMount() {
+        this.timerID = setInterval(() =>
+            this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
-setInterval(tick, 1000);
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
+
+
